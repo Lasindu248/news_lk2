6988577f900a71886aa631fc351d1430a4ba9705
nuuuwan <nuuuwan@gmail.com>
2022-03-07 18:30:40 +0530

    Replaced get_newspaper_id with newspaper_id property which is auto generated from class name.

--- src/news_lk2/core/AbstractNewsPaper.py
+++ src/news_lk2/core/AbstractNewsPaper.py
@@ -1,22 +1,23 @@
-
+from utils import dt
 from news_lk2.core.Article import Article
 
 class AbstractNewsPaper:
-    def get_newspaper_id():
-        raise NotImplementedError
-
     def get_article_d(self):
         raise NotImplementedError
 
     def get_article_urls(self):
         raise NotImplementedError
 
+    @property
+    def newspaper_id(self):
+        return dt.to_kebab(dt.camel_to_snake(self.__class__.__name__))
+
     def scrape(self):
         article_urls = self.get_article_urls()
         for article_url in article_urls:
             d = self.get_article_d(article_url)
             article = Article(
-                newspaper_id=self.get_newspaper_id(),
+                newspaper_id=self.newspaper_id,
                 url=article_url,
                 time_ut=d['time_ut'],
                 title=d['title'],
