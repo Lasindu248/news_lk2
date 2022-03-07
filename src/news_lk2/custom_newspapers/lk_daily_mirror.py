5fd7926e3afbadf2e9841c7c7927c49254d927b9
nuuuwan <nuuuwan@gmail.com>
2022-03-07 14:55:53 +0530

    Various

--- src/news_lk2/custom_newspapers/lk_daily_mirror.py
+++ src/news_lk2/custom_newspapers/lk_daily_mirror.py
@@ -1,36 +1,44 @@
-import os
 
 from bs4 import BeautifulSoup
-from utils import jsonx, www
+from utils import jsonx, timex, www
 
 from news_lk2._utils import log
+from news_lk2.core.article import get_article_file
+from news_lk2.core.common import init
 
-URL_NEWS = 'https://www.dailymirror.lk/news/209'
-DIR_OUTPUT = '/tmp/news_lk2'
+URL_NEWS = 'https://www.dailymirror.lk/latest-news/342'
+TIME_RAW_FORMAT = '%d %B %Y %I:%M %p'
+TIME_FORMAT = '%Y-%m-%d %H:%M'
+NEWSPAPER_NAME = 'lk_daily_mirror'
 
 
-def init():
-    if not os.path.exists(DIR_OUTPUT):
-        os.mkdir(DIR_OUTPUT)
+def parse_time(time_raw):
+    return timex.parse_time(time_raw, TIME_RAW_FORMAT)
 
 
-def scrape_article(url):
-    log.info(f'Scraping article {url}')
+def scrape_and_save_article(url):
+    log.debug(f'Scraping article {url}')
     html = www.read(url)
     soup = BeautifulSoup(html, 'html.parser')
     h1_title = soup.find('h1')
     span_time = soup.find('span', {'class': 'gtime'})
     header_inner = soup.find('header', {'class': 'inner-content'})
 
-    return {
+    time_ut = parse_time(span_time.text.strip())
+    time_str = timex.format_time(time_ut, TIME_FORMAT)
+    article = {
         'url': url,
         'title': h1_title.text.strip(),
-        'time_raw': span_time.text.strip(),
+        'time_ut': time_ut,
+        'time_str': time_str,
         'body_lines': list(map(
             lambda line: line.strip(),
             header_inner.text.strip().split('\n'),
         )),
     }
+    article_file = get_article_file(NEWSPAPER_NAME, article['time_ut'], url)
+    jsonx.write(article_file, article)
+    log.info(f'Wrote {article_file}')
 
 
 def scrape_index(index_url):
@@ -38,16 +46,9 @@ def scrape_index(index_url):
     html = www.read(index_url)
     soup = BeautifulSoup(html, 'html.parser')
 
-    article_list = []
     for div in soup.find_all('div', {'class': 'col-md-8'}):
         article_url = div.find('a').get('href')
-        article = scrape_article(article_url)
-        article_list.append(article)
-        break
-
-    article_list_file = '/tmp/news_lk2/article_list.json'
-    jsonx.write(article_list_file, article_list)
-    log.info(f'Wrote {article_list_file}')
+        scrape_and_save_article(article_url)
 
 
 if __name__ == '__main__':
