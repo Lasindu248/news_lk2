754060ed71be3968ab6e8f85c39c5894e06c2627
nuuuwan <nuuuwan@gmail.com>
2022-03-07 18:15:20 +0530

    Added AbstractNewsPaper with common NewsPaper logic

---
+++ src/news_lk2/core/AbstractNewsPaper.py
@@ -0,0 +1,30 @@
+
+from news_lk2.core.Article import Article
+
+URL_NEWS = 'https://www.dailymirror.lk/latest-news/342'
+TIME_RAW_FORMAT = '%d %B %Y %I:%M %p'
+NEWSPAPER_NAME = 'lk_daily_mirror'
+
+
+class AbstractNewsPaper:
+    def get_newspaper_id():
+        raise NotImplementedError
+
+    def get_article_d(self):
+        raise NotImplementedError
+
+    def get_article_urls(self):
+        raise NotImplementedError
+
+    def scrape(self):
+        article_urls = self.get_article_urls()
+        for article_url in article_urls:
+            d = self.get_article_d(article_url)
+            article = Article(
+                newspaper_id=self.get_newspaper_id(),
+                url=article_url,
+                time_ut=d['time_ut'],
+                title=d['title'],
+                body_lines=d['body_lines'],
+            )
+            article.store()
--- src/news_lk2/core/__init__.py
+++ src/news_lk2/core/__init__.py
@@ -1,1 +1,2 @@
+from news_lk2.core.AbstractNewsPaper import AbstractNewsPaper
 from news_lk2.core.Article import Article
