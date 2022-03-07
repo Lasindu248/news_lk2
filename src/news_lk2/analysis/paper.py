6db0cd500d0e74360e0b4ad543228d94866ae9af
nuuuwan <nuuuwan@gmail.com>
2022-03-07 16:51:28 +0530

    Added basic paper

--- src/news_lk2/analysis/paper.py
+++ src/news_lk2/analysis/paper.py
@@ -1,20 +1,77 @@
+import os
+import shutil
+from utils import timex, filex
+from news_lk2._utils import log
 from news_lk2.core import Article
-from news_lk2.core.filesys import (get_article_files_for_date_and_newspaper,
-                                   get_date_ids, get_newspapers_for_date,
-                                   git_checkout)
+from news_lk2.core.filesys import (
+    DIR_REPO,
+    git_checkout,
+    get_date_ids,
+    get_newspapers_for_date,
+    get_article_files_for_date_and_newspaper,
+)
+DELIM_MD = '/n/n'
+
+def get_articles_for_days_ago(days_ago=0):
+    date_ids = get_date_ids()
+    date_id = timex.get_date_id(
+        timex.get_unixtime() - timex.SECONDS_IN.DAY * days_ago,
+    )
+    if date_id not in date_ids:
+        log.warning(f'No articles for ({date_id}) missing from repo. Aborting')
+        return []
+
+    newspaper_names = get_newspapers_for_date(date_id)
+    article_list = []
+    for newspaper_name in newspaper_names:
+        print(' ' * 2, newspaper_name)
+        article_files = get_article_files_for_date_and_newspaper(
+            date_id,
+            newspaper_name,
+        )
+        article_list += list(map(
+            Article.load,
+            article_files,
+        ))
+
+    article_list.sort()
+    return article_list
+
+def get_dir_papers():
+    dir_papers = os.path.join(DIR_REPO, 'papers')
+    if not os.path.exists(dir_papers):
+        os.system(f'mkdir -p {dir_papers}')
+    return dir_papers
+
+def build_todays_paper():
+    date = timex.get_date()
+    date_id = timex.get_date_id()
+    todays_articles = get_articles_for_days_ago(0)
+    n_todays_articles = len(todays_articles)
+    md_lines = [f'# {date}']
+    md_lines = [f'*{n_todays_articles} news articles*']
+    for article in todays_articles:
+        print(article.body_lines)
+        md_lines += [
+            f'## {article.title}',
+            f'[{article.url}]',
+        ]
+        # md_lines += article.body_lines
+        break
+
+    print(md_lines)
+
+    md_file = os.path.join(
+        get_dir_papers(),
+        f'{date_id}.md',
+    )
+    filex.write(md_file, DELIM_MD.join(md_lines))
+    log.info(f'Wrote {md_file}')
+    readme_file = os.path.join(DIR_REPO, 'README.md')
+    shutil.copy(md_file, readme_file)
+
+
+
 
 if __name__ == '__main__':
-    git_checkout()
-    date_ids = get_date_ids()
-    for date_id in date_ids:
-        print(date_id)
-        newspaper_names = get_newspapers_for_date(date_id)
-        for newspaper_name in newspaper_names:
-            print(' ' * 2, newspaper_name)
-            article_files = get_article_files_for_date_and_newspaper(
-                date_id,
-                newspaper_name,
-            )
-            for article_file in article_files:
-                article = Article.load(article_file)
-                print(' ' * 4, article.title)
+    build_todays_paper()
