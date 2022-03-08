dfb8f4b8cce040571833acaf5a1d00bc0467c9a8
nuuuwan <nuuuwan@gmail.com>
2022-03-08 08:26:35 +0530

    Removed paper daily job (replaced by gh-pages)

--- src/news_lk2/upload_data.py
+++ src/news_lk2/upload_data.py
@@ -4,4 +4,3 @@ from news_lk2.custom_newspapers import newspaper_class_list
 if __name__ == '__main__':
     for newspaper_class in newspaper_class_list:
         newspaper_class.scrape()
-    paper.run_daily_job()
