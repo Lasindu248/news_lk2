188958fdce52d02e0838ea15eabef35618eb855a
nuuuwan <nuuuwan@gmail.com>
2022-03-08 14:19:40 +0530

    Added get_timezone_correction

--- src/news_lk2/_utils.py
+++ src/news_lk2/_utils.py
@@ -1,5 +1,12 @@
 """Utils."""
+import time
 
 from utils import logx
 
 log = logx.get_logger('news_lk2')
+
+TIMEZONE_SRI_LANKA = 19800
+
+
+def get_timezone_correction():
+    return time.timezone + TIMEZONE_SRI_LANKA
