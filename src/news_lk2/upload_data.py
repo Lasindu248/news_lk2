d2b19da5de1abab33f521a565c83217aae9e9b90
nuuuwan <nuuuwan@gmail.com>
2022-03-08 13:40:30 +0530

    Added git_checkout to upload_data

--- src/news_lk2/upload_data.py
+++ src/news_lk2/upload_data.py
@@ -1,5 +1,7 @@
+from news_lk2.core.filesys import git_checkout
 from news_lk2.custom_newspapers import newspaper_class_list
 
 if __name__ == '__main__':
+    git_checkout()
     for newspaper_class in newspaper_class_list:
         newspaper_class.scrape()
