93edec0a55207b78d6bae38e265c210e65674444
nuuuwan <nuuuwan@gmail.com>
2022-03-08 19:41:01 +0530

    Added AdaDeranaLk to __init__

--- src/news_lk2/custom_newspapers/__init__.py
+++ src/news_lk2/custom_newspapers/__init__.py
@@ -1,11 +1,13 @@
+from news_lk2.custom_newspapers.AdaDeranaLk import AdaDeranaLk
 from news_lk2.custom_newspapers.DailyFtLk import DailyFtLk
 from news_lk2.custom_newspapers.DailyMirrorLk import DailyMirrorLk
 from news_lk2.custom_newspapers.DailyNewsLk import DailyNewsLk
 from news_lk2.custom_newspapers.IslandLk import IslandLk
 
 newspaper_class_list = [
-    DailyNewsLk,
+    AdaDeranaLk,
     DailyFtLk,
     DailyMirrorLk,
+    DailyNewsLk,
     IslandLk,
 ]
