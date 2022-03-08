1fb71dc5dca4210ecace91937ef6fb1700292e83
nuuuwan <nuuuwan@gmail.com>
2022-03-08 09:14:42 +0530

    N_BACKPOPULATE = 366

--- src/news_lk2/gh-pages/build.py
+++ src/news_lk2/gh-pages/build.py
@@ -11,7 +11,7 @@ from news_lk2.core.filesys import (DIR_REPO, DIR_ROOT, get_date_ids,
 
 DIR_GH_PAGES = os.path.join(DIR_ROOT, f'{DIR_REPO}-gh-pages')
 FORMAT_DATE_TITLE = '%A, %B %d, %Y'
-N_BACKPOPULATE = 3
+N_BACKPOPULATE = 366
 
 
 def clean():
