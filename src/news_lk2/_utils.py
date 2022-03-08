0d7e1b1de14e696377b34b98e8abf55490148739
nuuuwan <nuuuwan@gmail.com>
2022-03-08 17:42:04 +0530

    Removed get_seconds_behind_sl

--- src/news_lk2/_utils.py
+++ src/news_lk2/_utils.py
@@ -1,12 +1,5 @@
 """Utils."""
-import time
 
 from utils import logx
 
 log = logx.get_logger('news_lk2')
-
-TIMEZONE_SRI_LANKA = 19800
-
-
-def get_seconds_behind_sl():
-    return time.timezone + TIMEZONE_SRI_LANKA
