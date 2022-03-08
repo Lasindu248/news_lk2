71465456fc9d83c005368fd7646d23a7989a5ea7
nuuuwan <nuuuwan@gmail.com>
2022-03-08 08:19:04 +0530

    Force git checkout

--- src/news_lk2/analysis/paper.py
+++ src/news_lk2/analysis/paper.py
@@ -7,7 +7,8 @@ from news_lk2._utils import log
 from news_lk2.core import Article
 from news_lk2.core.filesys import (DIR_REPO,
                                    get_article_files_for_date_and_newspaper,
-                                   get_date_ids, get_newspapers_for_date)
+                                   get_date_ids, get_newspapers_for_date,
+                                   git_checkout)
 
 DELIM_MD = '\n\n'
 
@@ -77,6 +78,7 @@ def copy_to_readme(md_file):
 
 
 def run_daily_job():
+    git_checkout()
     build_paper(2)
     build_paper(1)
     md_file = build_paper(0)
