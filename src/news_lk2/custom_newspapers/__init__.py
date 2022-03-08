8b86a9eeb73646b2a23fa49c6e327450f2ea6741
nuuuwan <nuuuwan@gmail.com>
2022-03-08 19:51:45 +0530

    Added EconomyNextCom

--- src/news_lk2/custom_newspapers/__init__.py
+++ src/news_lk2/custom_newspapers/__init__.py
@@ -2,10 +2,12 @@ from news_lk2.custom_newspapers.AdaDeranaLk import AdaDeranaLk
 from news_lk2.custom_newspapers.DailyFtLk import DailyFtLk
 from news_lk2.custom_newspapers.DailyMirrorLk import DailyMirrorLk
 from news_lk2.custom_newspapers.DailyNewsLk import DailyNewsLk
+from news_lk2.custom_newspapers.EconomyNextCom import EconomyNextCom
 from news_lk2.custom_newspapers.IslandLk import IslandLk
 
 newspaper_class_list = [
     AdaDeranaLk,
+    EconomyNextCom,
     DailyFtLk,
     DailyMirrorLk,
     DailyNewsLk,
