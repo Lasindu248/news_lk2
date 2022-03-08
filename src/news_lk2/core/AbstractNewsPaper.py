c8f86e674a9275bb003eb09903f87291ba89f9ba
nuuuwan <nuuuwan@gmail.com>
2022-03-08 13:40:58 +0530

    Added check not to rescrape scraped files; Refactored parse_and_store_article;

--- src/news_lk2/core/AbstractNewsPaper.py
+++ src/news_lk2/core/AbstractNewsPaper.py
@@ -1,9 +1,10 @@
 from abc import ABC
-
+import os
 from bs4 import BeautifulSoup
 from utils import dt, www
 
 from news_lk2._utils import log
+from news_lk2.core.filesys import get_article_file
 from news_lk2.core.Article import Article
 
 MIN_ARTICLE_HTML_SIZE = 1_000
@@ -58,24 +59,29 @@ class AbstractNewsPaper(ABC):
 
     @classmethod
     def parse_and_store_article(cls, article_url):
+        article_file = get_article_file(article_url)
+        if os.path.exists(article_file):
+            log.info(f'{article_file} already exists. Not parsing.')
+            return
+
         html = www.read(article_url)
-        if is_html_valid(html):
-            soup = BeautifulSoup(html, 'html.parser')
-            article = Article(
-                newspaper_id=cls.get_newspaper_id(),
-                url=article_url,
-                time_ut=cls.parse_time_ut(soup),
-                title=cls.parse_title(soup),
-                body_lines=cls.parse_body_lines(soup),
-            )
-            article.store()
+        if not is_html_valid(html):
+            log.warn(f'{article_file} has invalid HTML. Not parsing.')
+            return
+
+        soup = BeautifulSoup(html, 'html.parser')
+        article = Article(
+            newspaper_id=cls.get_newspaper_id(),
+            url=article_url,
+            time_ut=cls.parse_time_ut(soup),
+            title=cls.parse_title(soup),
+            body_lines=cls.parse_body_lines(soup),
+        )
+        article.store()
+
 
     @classmethod
     def scrape(cls):
-        try:
-            article_urls = cls.get_article_urls()
-            for article_url in article_urls:
-                cls.parse_and_store_article(article_url)
-        except Exception:
-            newspaper_id = cls.get_newspaper_id()
-            log.warning(f'Could not complete scrape for {newspaper_id}')
+        article_urls = cls.get_article_urls()
+        for article_url in article_urls:
+            cls.parse_and_store_article(article_url)
