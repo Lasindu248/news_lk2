0364e5b725d924dba25e5a4c62140f2c3044c5ff
nuuuwan <nuuuwan@gmail.com>
2022-03-10 21:03:48 +0530

    Modified to be single file

--- src/news_lk2/gh-pages/build.py
+++ src/news_lk2/gh-pages/build.py
@@ -6,7 +6,7 @@ from utils.xmlx import _
 
 from news_lk2._utils import log
 from news_lk2.analysis.ner import render_line
-from news_lk2.analysis.paper import dedupe_by_title, get_date_id_to_articles
+from news_lk2.analysis.paper import dedupe_by_title, get_articles
 from news_lk2.core.filesys import DIR_REPO, DIR_ROOT, git_checkout
 
 DIR_GH_PAGES = os.path.join(DIR_ROOT, f'{DIR_REPO}-gh-pages')
@@ -55,16 +55,19 @@ def render_article(article):
     )), {'class': 'div-article'})
 
 
-def build_paper(date_id_to_articles, date_id):
-    days_articles = date_id_to_articles[date_id]
-    days_articles = dedupe_by_title(days_articles)
-    days_articles.reverse()
+def build_paper():
     ut = timex.get_unixtime()
-    n_days_articles = len(days_articles)
+    ut_min = ut - MAX_DAYS_AGO * timex.SECONDS_IN.DAY
+
+    articles = get_articles(ut_min=ut_min)
+    articles = dedupe_by_title(articles)
+    articles.reverse()
+
+    n_articles = len(articles)
 
     rendered_articles = list(map(
         render_article,
-        days_articles,
+        articles,
     ))
 
     time_last_updated = timex.format_time(
@@ -86,25 +89,9 @@ def build_paper(date_id_to_articles, date_id):
         _('div', rendered_articles),
     ])
     html = _('html', [head, body])
-    file_only = get_date_file_only(date_id)
-    html_file = os.path.join(DIR_GH_PAGES, file_only)
-    html.store(html_file)
-    log.info(f'Stored {html_file} ({n_days_articles}  articles)')
-
-    return file_only
-
-
-def build_index_file(html_file):
-    head = _('head', [
-        _('meta', None, {
-            'http-equiv': 'refresh',
-            'content': f'0; URL={html_file}',
-        }),
-    ])
-    html = _('html', [head])
     html_file = os.path.join(DIR_GH_PAGES, 'index.html')
     html.store(html_file)
-    log.info(f'Stored {html_file}')
+    log.info(f'Stored {html_file} ({n_articles}  articles)')
 
 
 def copy_css_file():
@@ -117,11 +104,7 @@ def copy_css_file():
 def build():
     clean()
     git_checkout()
-    html_file = None
-    date_id_to_articles = get_date_id_to_articles(max_days_ago=MAX_DAYS_AGO)
-    for date_id in date_id_to_articles:
-        html_file = build_paper(date_id_to_articles, date_id)
-    build_index_file(html_file)
+    build_paper()
     copy_css_file()
 
 
