5cda15385ef1228bcb7c352befed4d024a81bbf2
nuuuwan <nuuuwan@gmail.com>
2022-03-10 19:04:17 +0530

    Combined NER Text

--- src/news_lk2/analysis/ner.py
+++ src/news_lk2/analysis/ner.py
@@ -6,11 +6,34 @@ nlp = spacy.load("en_core_web_sm")
 
 def render_line(line):
     doc = nlp(line)
-    return _('span', list(map(
-        lambda token: _(
-            'span',
-            token.text,
-            {'class': f'span-nlp span-nlp-{token.pos_.lower()}'},
-        ),
-        [token for token in doc],
-    )))
+    prev_pos = None
+    current_segment = None
+    rendered_segments = []
+    for token in doc:
+        pos = token.pos_.lower()
+        text = token.text
+        if pos != prev_pos:
+            rendered_segments.append(
+                _(
+                    'span',
+                    current_segment,
+                    {'class': f'span-nlp span-nlp-{prev_pos}'},
+                )
+            )
+            current_segment = None
+
+        if current_segment is None:
+            current_segment = text
+        else:
+            current_segment += ' ' + text
+        prev_pos = pos
+
+    if current_segment:
+        rendered_segments.append(
+            _(
+                'span',
+                text,
+                {'class': f'span-nlp span-nlp-{prev_pos}'},
+            )
+        )
+    return _('span', rendered_segments)
