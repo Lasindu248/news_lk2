e59112f71244f33b0c9c39e70dc73e2d50440f86
nuuuwan <nuuuwan@gmail.com>
2022-03-11 13:59:58 +0530

    Changed rate and lang;

--- src/news_lk2/gh-pages/index.js
+++ src/news_lk2/gh-pages/index.js
@@ -1,6 +1,10 @@
 function tts(text) {
     let synth = window.speechSynthesis;
     synth.cancel();
+
     let utterThis = new SpeechSynthesisUtterance(text);
+    utterThis.lang = 'en-GB';
+    utterThis.rate = 1.25;
+
     synth.speak(utterThis);
 }
