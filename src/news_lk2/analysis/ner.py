74ff960714b4573d6c6d17a3351fbfb7906e3b9e
nuuuwan <nuuuwan@gmail.com>
2022-03-11 13:32:41 +0530

    Added get_combined_nlp + supporting code

--- src/news_lk2/analysis/ner.py
+++ src/news_lk2/analysis/ner.py
@@ -1,7 +1,56 @@
+import re
+
 import spacy
 from utils.xmlx import _
 
 nlp = spacy.load("en_core_web_sm")
+
+
+class POS:
+    NE = 'propn'
+    OTHER = 'other'
+    PUNCT = 'punct'
+    NUM = 'num'
+
+
+def clean(text):
+    return re.sub(r'\s+', ' ', text).strip()
+
+
+def get_pos(spacy_pos):
+    if spacy_pos in ['PROPN']:
+        return POS.NE
+    if spacy_pos in ['NUM']:
+        return POS.NUM
+    if spacy_pos in ['PUNCT']:
+        return POS.PUNCT
+    return POS.OTHER
+
+
+def get_combined_nlp(text):
+    doc = nlp(clean(text))
+    prev_pos = None
+    combined_nlp = []
+    current_token_texts = []
+    for token in doc:
+        pos = get_pos(token.pos_)
+        text = token.text
+
+        if prev_pos and pos != prev_pos:
+            combined_nlp.append({
+                'pos': prev_pos,
+                'text': ' '.join(current_token_texts),
+            })
+            current_token_texts = []
+        current_token_texts.append(text)
+        prev_pos = pos
+
+    if current_token_texts:
+        combined_nlp.append({
+            'pos': prev_pos,
+            'text': ' '.join(current_token_texts),
+        })
+    return combined_nlp
 
 
 def render_line(line):
