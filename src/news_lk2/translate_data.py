86c3bab294fce2c8c11b3973bd06de3a340f2311
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-05-31 11:39:45 +0530

    Script for translating data

---
+++ src/news_lk2/translate_data.py
@@ -0,0 +1,16 @@
+from news_lk2.analysis.paper import get_articles
+from news_lk2.core import TranslatedArticle
+from news_lk2.core.filesys import git_checkout
+
+MAX_ARTICLES_TO_TRANSLATE = 20
+
+if __name__ == '__main__':
+    git_checkout()
+    articles = get_articles()
+    i_translated = 0
+    for article in articles:
+        translated_article = TranslatedArticle.initFromArticle(article)
+        if translated_article.store():
+            i_translated += 1
+        if i_translated >= MAX_ARTICLES_TO_TRANSLATE:
+            break
